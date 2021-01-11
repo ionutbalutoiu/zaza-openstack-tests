43b8162265150545e6b562e3d956022df41fb72d
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-01-11 15:46:40 +0200

    backwards-compat

--- zaza/openstack/charm_tests/saml_mellon/setup.py
+++ zaza/openstack/charm_tests/saml_mellon/setup.py
@@ -15,10 +15,11 @@
 """Code for setting up keystone federation."""
 
 import json
+import keystoneauth1
+import os
 import tempfile
 
-import keystoneauth1
-
+import zaza.charm_lifecycle.utils as charm_lifecycle_utils
 import zaza.model
 from zaza.openstack.utilities import (
     cert as cert_utils,
@@ -26,9 +27,16 @@ from zaza.openstack.utilities import (
     openstack as openstack_utils,
 )
 
+
+APP_NAME = "keystone-saml-mellon"
+
+FEDERATED_DOMAIN = "federated_domain"
+FEDERATED_GROUP = "federated_users"
 MEMBER = "Member"
+IDP = "samltest"
+LOCAL_IDP_REMOTE_ID = "http://{}/simplesaml/saml2/idp/metadata.php"
+REMOTE_ID = "https://samltest.id/saml/idp"
 PROTOCOL_NAME = "mapped"
-IDP_REMOTE_ID = "http://{}/simplesaml/saml2/idp/metadata.php"
 MAP_TEMPLATE = '''
     [{{
             "local": [
@@ -73,8 +81,10 @@ SP_SIGNING_KEY_INFO_XML_TEMPLATE = '''
 '''
 
 
-def _keystone_federation_setup(federated_domain=None, federated_group=None,
-                               idp_name=None, idp_remote_id=None):
+def keystone_federation_setup(federated_domain=FEDERATED_DOMAIN,
+                              federated_group=FEDERATED_GROUP,
+                              idp_name=IDP,
+                              idp_remote_id=REMOTE_ID):
     """Configure Keystone Federation."""
     cli_utils.setup_logging()
     keystone_session = openstack_utils.get_overcloud_keystone_session()
@@ -127,14 +137,39 @@ def _keystone_federation_setup(federated_domain=None, federated_group=None,
             PROTOCOL_NAME, mapping=map_name, identity_provider=idp)
 
 
-def _attach_saml_resources(keystone_saml_mellon_app_name=None,
-                           test_saml_idp_app_name=None):
-    """Attach resources to the Keystone SAML Mellon and the local IdP."""
+def attach_saml_resources(application="keystone-saml-mellon"):
+    """Attach resource to the Keystone SAML Mellon charm."""
+    test_idp_metadata_xml = "samltest.xml"
+    idp_metadata_xml_file = os.path.join(
+        charm_lifecycle_utils.BUNDLE_DIR, test_idp_metadata_xml)
+
     idp_metadata_name = "idp-metadata"
-    sp_metadata_name = "sp-metadata"
     sp_private_key_name = "sp-private-key"
     sp_signing_keyinfo_name = "sp-signing-keyinfo"
 
+    zaza.model.attach_resource(
+        application, idp_metadata_name, idp_metadata_xml_file)
+
+    (key, cert) = cert_utils.generate_cert('SP Signing Key')
+
+    cert = cert.decode().strip("-----BEGIN CERTIFICATE-----")
+    cert = cert.strip("-----END CERTIFICATE-----")
+
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.pem') as fp:
+        fp.write(key.decode())
+        fp.flush()
+        zaza.model.attach_resource(application, sp_private_key_name, fp.name)
+
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.xml') as fp:
+        fp.write(SP_SIGNING_KEY_INFO_XML_TEMPLATE.format(cert))
+        fp.flush()
+        zaza.model.attach_resource(
+            application, sp_signing_keyinfo_name, fp.name)
+
+
+def _attach_saml_resources_local_idp(keystone_saml_mellon_app_name=None,
+                                     test_saml_idp_app_name=None):
+    """Attach resources to the Keystone SAML Mellon and the local IdP."""
     test_saml_idp_unit = zaza.model.get_units(
         test_saml_idp_app_name).pop()
     action_result = zaza.model.run_action(
@@ -145,7 +180,7 @@ def _attach_saml_resources(keystone_saml_mellon_app_name=None,
         fp.write(idp_metadata)
         fp.flush()
         zaza.model.attach_resource(
-            keystone_saml_mellon_app_name, idp_metadata_name, fp.name)
+            keystone_saml_mellon_app_name, "idp-metadata", fp.name)
 
     (key, cert) = cert_utils.generate_cert('SP Signing Key')
 
@@ -156,13 +191,13 @@ def _attach_saml_resources(keystone_saml_mellon_app_name=None,
         fp.write(key.decode())
         fp.flush()
         zaza.model.attach_resource(
-            keystone_saml_mellon_app_name, sp_private_key_name, fp.name)
+            keystone_saml_mellon_app_name, 'sp-private-key', fp.name)
 
     with tempfile.NamedTemporaryFile(mode='w', suffix='.xml') as fp:
         fp.write(SP_SIGNING_KEY_INFO_XML_TEMPLATE.format(cert))
         fp.flush()
         zaza.model.attach_resource(
-            keystone_saml_mellon_app_name, sp_signing_keyinfo_name, fp.name)
+            keystone_saml_mellon_app_name, 'sp-signing-keyinfo', fp.name)
 
     keystone_saml_mellon_unit = zaza.model.get_units(
         keystone_saml_mellon_app_name).pop()
@@ -174,19 +209,19 @@ def _attach_saml_resources(keystone_saml_mellon_app_name=None,
         fp.write(sp_metadata)
         fp.flush()
         zaza.model.attach_resource(
-            test_saml_idp_app_name, sp_metadata_name, fp.name)
+            test_saml_idp_app_name, 'sp-metadata', fp.name)
 
 
 def attach_saml_resources_idp1():
     """Attach the SAML resources for the local IdP #1."""
-    _attach_saml_resources(
+    _attach_saml_resources_local_idp(
         keystone_saml_mellon_app_name="keystone-saml-mellon1",
         test_saml_idp_app_name="test-saml-idp1")
 
 
 def attach_saml_resources_idp2():
     """Attach the SAML resources for the local IdP #2."""
-    _attach_saml_resources(
+    _attach_saml_resources_local_idp(
         keystone_saml_mellon_app_name="keystone-saml-mellon2",
         test_saml_idp_app_name="test-saml-idp2")
 
@@ -194,10 +229,10 @@ def attach_saml_resources_idp2():
 def keystone_federation_setup_idp1():
     """Configure Keystone Federation for the local IdP #1."""
     test_saml_idp_unit = zaza.model.get_units("test-saml-idp1").pop()
-    idp_remote_id = IDP_REMOTE_ID.format(
+    idp_remote_id = LOCAL_IDP_REMOTE_ID.format(
         test_saml_idp_unit.data['public-address'])
 
-    _keystone_federation_setup(
+    keystone_federation_setup(
         federated_domain="federated_domain_idp1",
         federated_group="federated_users_idp1",
         idp_name="test-saml-idp1",
@@ -207,10 +242,10 @@ def keystone_federation_setup_idp1():
 def keystone_federation_setup_idp2():
     """Configure Keystone Federation for the local IdP #2."""
     test_saml_idp_unit = zaza.model.get_units("test-saml-idp2").pop()
-    idp_remote_id = IDP_REMOTE_ID.format(
+    idp_remote_id = LOCAL_IDP_REMOTE_ID.format(
         test_saml_idp_unit.data['public-address'])
 
-    _keystone_federation_setup(
+    keystone_federation_setup(
         federated_domain="federated_domain_idp2",
         federated_group="federated_users_idp2",
         idp_name="test-saml-idp2",
--- zaza/openstack/charm_tests/saml_mellon/tests.py
+++ zaza/openstack/charm_tests/saml_mellon/tests.py
@@ -20,11 +20,142 @@ import requests
 
 import zaza.model
 from zaza.openstack.charm_tests.keystone import BaseKeystoneTest
+import zaza.charm_lifecycle.utils as lifecycle_utils
 import zaza.openstack.utilities.openstack as openstack_utils
 
 
 class FailedToReachIDP(Exception):
     """Custom Exception for failing to reach the IDP."""
+
+    pass
+
+
+class CharmKeystoneSAMLMellonTest(BaseKeystoneTest):
+    """Charm Keystone SAML Mellon tests."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Run class setup for running Keystone SAML Mellon charm tests."""
+        super(CharmKeystoneSAMLMellonTest, cls).setUpClass()
+        # Note: The BaseKeystoneTest class sets the application_name to
+        # "keystone" which breaks keystone-saml-mellon actions. Explicitly set
+        # application name here.
+        cls.test_config = lifecycle_utils.get_charm_config()
+        cls.application_name = cls.test_config['charm_name']
+        cls.action = "get-sp-metadata"
+        cls.current_release = openstack_utils.get_os_release()
+        cls.FOCAL_USSURI = openstack_utils.get_os_release("focal_ussuri")
+
+    def test_run_get_sp_metadata_action(self):
+        """Validate the get-sp-metadata action."""
+        unit = zaza.model.get_units(self.application_name)[0]
+        if self.vip:
+            ip = self.vip
+        else:
+            ip = unit.public_address
+
+        action = zaza.model.run_action(unit.entity_id, self.action)
+        if "failed" in action.data["status"]:
+            raise Exception(
+                "The action failed: {}".format(action.data["message"]))
+
+        output = action.data["results"]["output"]
+        root = etree.fromstring(output)
+        for item in root.items():
+            if "entityID" in item[0]:
+                assert ip in item[1]
+
+        for appt in root.getchildren():
+            for elem in appt.getchildren():
+                for item in elem.items():
+                    if "Location" in item[0]:
+                        assert ip in item[1]
+
+        logging.info("Successul get-sp-metadata action")
+
+    def test_saml_mellon_redirects(self):
+        """Validate the horizon -> keystone -> IDP redirects."""
+        if self.vip:
+            keystone_ip = self.vip
+        else:
+            unit = zaza.model.get_units(self.application_name)[0]
+            keystone_ip = unit.public_address
+
+        horizon = "openstack-dashboard"
+        horizon_vip = (zaza.model.get_application_config(horizon)
+                       .get("vip").get("value"))
+        if horizon_vip:
+            horizon_ip = horizon_vip
+        else:
+            unit = zaza.model.get_units("openstack-dashboard")[0]
+            horizon_ip = unit.public_address
+
+        if self.tls_rid:
+            proto = "https"
+        else:
+            proto = "http"
+
+        # Use Keystone URL for < Focal
+        if self.current_release < self.FOCAL_USSURI:
+            region = "{}://{}:5000/v3".format(proto, keystone_ip)
+        else:
+            region = "default"
+
+        url = "{}://{}/horizon/auth/login/".format(proto, horizon_ip)
+        horizon_expect = ('<option value="samltest_mapped">'
+                          'samltest.id</option>')
+
+        # This is the message samltest.id gives when it has not had
+        # SP XML uploaded. It still shows we have been directed to:
+        # horizon -> keystone -> samltest.id
+        idp_expect = ("The application you have accessed is not registered "
+                      "for use with this service.")
+
+        def _do_redirect_check(url, region, idp_expect, horizon_expect):
+
+            # start session, get csrftoken
+            client = requests.session()
+            # Verify=False see note below
+            login_page = client.get(url, verify=False)
+
+            # Validate SAML method is available
+            assert horizon_expect in login_page.text
+
+            # Get cookie
+            if "csrftoken" in client.cookies:
+                csrftoken = client.cookies["csrftoken"]
+            else:
+                raise Exception("Missing csrftoken")
+
+            # Build and send post request
+            form_data = {
+                "auth_type": "samltest_mapped",
+                "csrfmiddlewaretoken": csrftoken,
+                "next": "/horizon/project/api_access",
+                "region": region,
+            }
+
+            # Verify=False due to CA certificate bundles.
+            # If we point to the CA for keystone/horizon they work but
+            # samltest.id does not.
+            # If we don't set it validation fails for keystone/horizon
+            # We would have to install the keystone CA onto the system
+            # to validate end to end.
+            response = client.post(
+                url, data=form_data,
+                headers={"Referer": url},
+                allow_redirects=True,
+                verify=False)
+
+            if idp_expect not in response.text:
+                msg = "FAILURE code={} text={}".format(response, response.text)
+                # Raise a custom exception.
+                raise FailedToReachIDP(msg)
+
+        # Execute the check
+        # We may need to try/except to allow horizon to build its pages
+        _do_redirect_check(url, region, idp_expect, horizon_expect)
+        logging.info("SUCCESS")
 
 
 class BaseCharmKeystoneSAMLMellonTest(BaseKeystoneTest):
